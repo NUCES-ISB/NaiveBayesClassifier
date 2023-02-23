78aac74898a90eee2cb774eba96fcbeee03667c5
Taimoor <120190987+itaimoor@users.noreply.github.com>
2023-02-24 00:57:32 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 if not os.path.exists('itaimoor/example.txt'):
+    print('Success: example.txt found.')
+else:
+
     print('Error: example.txt not found!')
     sys.exit(1)
-else:
-    print('Success: example.txt found.')

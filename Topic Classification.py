1a8fa25048bc34a0fb6eccce3939a14d9e8223bf
Taimoor <120190987+itaimoor@users.noreply.github.com>
2023-02-24 19:50:21 +0500

    Update Topic Classification.py

--- Topic Classification.py
+++ Topic Classification.py
@@ -1,95 +1,63 @@
 import pandas as pd
 import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from sklearn.preprocessing import LabelEncoder
+import nltk
 from collections import defaultdict
-from nltk.corpus import wordnet as wn
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection, naive_bayes, svm
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 
-#Set Random seed
+# Set random seed
 np.random.seed(500)
 
-# Add the Data using pandas
-Corpus = pd.read_csv(r"C:\Users\gunjit.bedi\Desktop\NLP Project\corpus_small.csv",encoding='latin-1')
+# Read data using pandas
+Corpus = pd.read_csv(r"C:\Users\gunjit.bedi\Desktop\NLP Project\corpus_small.csv", encoding='latin-1')
 
-# Step - 1: Data Pre-processing - This will help in getting better results through the classification algorithms
+# Preprocessing: Remove blank rows, change to lowercase, tokenize
+Corpus.dropna(subset=['text'], inplace=True)
+Corpus['text'] = Corpus['text'].str.lower()
+Corpus['text'] = Corpus['text'].apply(nltk.word_tokenize)
 
-# Step - 1a : Remove blank rows if any.
-Corpus['text'].dropna(inplace=True)
+# Preprocessing: Remove stop words, perform word lemmatization
+lemmatizer = nltk.stem.WordNetLemmatizer()
+tag_map = defaultdict(lambda: nltk.corpus.wordnet.NOUN)
+tag_map['J'] = nltk.corpus.wordnet.ADJ
+tag_map['V'] = nltk.corpus.wordnet.VERB
+tag_map['R'] = nltk.corpus.wordnet.ADV
+stop_words = set(nltk.corpus.stopwords.words('english'))
 
-# Step - 1b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
-Corpus['text'] = [entry.lower() for entry in Corpus['text']]
+def process_text(text):
+    final_words = []
+    for word, tag in nltk.pos_tag(text):
+        if word.isalpha() and word not in stop_words:
+            word_final = lemmatizer.lemmatize(word, tag_map[tag[0]])
+            final_words.append(word_final)
+    return ' '.join(final_words)
 
-# Step - 1c : Tokenization : In this each entry in the corpus will be broken into set of words
-Corpus['text']= [word_tokenize(entry) for entry in Corpus['text']]
+Corpus['text_final'] = Corpus['text'].apply(process_text)
 
-# Step - 1d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
+# Split the data into training and test sets
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'], Corpus['label'], test_size=0.3)
 
-# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
-tag_map = defaultdict(lambda : wn.NOUN)
-tag_map['J'] = wn.ADJ
-tag_map['V'] = wn.VERB
-tag_map['R'] = wn.ADV
-
-
-for index,entry in enumerate(Corpus['text']):
-    # Declaring Empty List to store the words that follow the rules for this step
-    Final_words = []
-    # Initializing WordNetLemmatizer()
-    word_Lemmatized = WordNetLemmatizer()
-    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
-    for word, tag in pos_tag(entry):
-        # Below condition is to check for Stop words and consider only alphabets
-        if word not in stopwords.words('english') and word.isalpha():
-            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-            Final_words.append(word_Final)
-    # The final processed set of words for each iteration will be stored in 'text_final'
-    Corpus.loc[index,'text_final'] = str(Final_words)
-
-#print(Corpus['text_final'].head())
-
-# Step - 2: Split the model into Train and Test Data set
-Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['label'],test_size=0.3)
-
-# Step - 3: Label encode the target variable  - This is done to transform Categorical data of string type in the data set into numerical values
-Encoder = LabelEncoder()
+# Label encode the target variable
+Encoder = model_selection.LabelEncoder()
 Train_Y = Encoder.fit_transform(Train_Y)
-Test_Y = Encoder.fit_transform(Test_Y)
+Test_Y = Encoder.transform(Test_Y)
 
-# Step - 4: Vectorize the words by using TF-IDF Vectorizer - This is done to find how important a word in document is in comaprison to the corpus
+# Vectorize the words using TF-IDF
 Tfidf_vect = TfidfVectorizer(max_features=5000)
 Tfidf_vect.fit(Corpus['text_final'])
 
 Train_X_Tfidf = Tfidf_vect.transform(Train_X)
 Test_X_Tfidf = Tfidf_vect.transform(Test_X)
 
+# Train and test the classifiers
+models = [
+    ('Naive Bayes', naive_bayes.MultinomialNB()),
+    ('SVM', svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto'))
+]
 
-# Step - 5: Now we can run different algorithms to classify out data check for accuracy
-
-# Classifier - Algorithm - Naive Bayes
-# fit the training dataset on the classifier
-Naive = naive_bayes.MultinomialNB()
-Naive.fit(Train_X_Tfidf,Train_Y)
-
-# predict the labels on validation dataset
-predictions_NB = Naive.predict(Test_X_Tfidf)
-
-# Use accuracy_score function to get the accuracy
-print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, Test_Y)*100)
-
-
-# Classifier - Algorithm - SVM
-# fit the training dataset on the classifier
-SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-SVM.fit(Train_X_Tfidf,Train_Y)
-
-# predict the labels on validation dataset
-predictions_SVM = SVM.predict(Test_X_Tfidf)
-
-# Use accuracy_score function to get the accuracy
-print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
+for name, model in models:
+    model.fit(Train_X_Tfidf, Train_Y)
+    predictions = model.predict(Test_X_Tfidf)
+    accuracy = accuracy_score(predictions, Test_Y) * 100
+    print(f"{name} accuracy score: {accuracy:.2f}%")
